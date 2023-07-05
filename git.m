388322f7efa362a85e35031d1b6fc8a14caa42d4
Ryan-Miyakawa <ryan@hooktheory.com>
2023-07-05 11:14:06 -0700

    adding mat2gds

--- git.m
+++ git.m
@@ -1,3 +1,13 @@
 
 function git(varargin)
-system(['git ', strjoin(varargin, ' ')]);
+celAsVar = varargin;
+for i = 1:numel(celAsVar)
+    % Check if the element contains multiple words (space-separated)
+    if contains(celAsVar{i}, ' ')
+        % Add double quotes to the element
+        celAsVar{i} = ['"', celAsVar{i}, '"'];
+    end
+end
+cmd = strjoin(celAsVar, ' ');
+
+system(['git ', cmd]);
